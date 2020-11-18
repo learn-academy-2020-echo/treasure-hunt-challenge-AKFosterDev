e74a822f9c8eb8efb19781e1e681d6ad3c86b698
AKFosterDev <akfoster2015@gmail.com>
2020-11-18 10:54:15 -0800

    complete first user story

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react'
+import Square from './components/Square'
 import './App.css'
 
-class App extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      squares: ["?", "?", "?", "?", "?", "?", "?", "?", "?"]
-    }
-  }
+class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			squares: ['?', '?', '?', '?', '?', '?', '?', '?', '?'],
+		}
+	}
 
-  render(){
-    return(
-      <React.Fragment>
-        <h1>Treasure Hunt App</h1>
-      </React.Fragment>
-    )
-  }
+	render() {
+		// destructure variable from state
+		let { squares } = this.state
+		return (
+			<React.Fragment>
+				<h1>Treasure Hunt App</h1>
+				<div id='gameboard'>
+					{squares.map((value, index) => {
+						return <Square value={value} key={index} />
+					})}
+				</div>
+			</React.Fragment>
+		)
+	}
 }
 export default App
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 
-class Square extends Component{
-  render(){
-    return(
-      <React.Fragment>
-      </React.Fragment>
-    )
-  }
+class Square extends Component {
+	render() {
+		let { value } = this.props
+		return (
+			<React.Fragment>
+				<div id='square'>{value}</div>
+			</React.Fragment>
+		)
+	}
 }
 export default Square
